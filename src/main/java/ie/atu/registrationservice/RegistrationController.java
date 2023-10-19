c9e80149f255efcd5729389bbd00490e615e57bf
DanKos22 <G00397054@atu.ie>
2023-10-20 02:52:30 +0300

    Updated code

--- src/main/java/ie/atu/registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/registrationservice/RegistrationController.java
@@ -1,11 +1,19 @@
 package ie.atu.registrationservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
+
+    private NotificationServiceClient notificationServiceClient;
+
+    @Autowired
+    public RegistrationController(NotificationServiceClient notificationServiceClient){
+        this.notificationServiceClient = notificationServiceClient;
+    }
 
     // Define a Registration Endpoint
     @PostMapping("/confirm")
@@ -14,5 +22,12 @@ public class RegistrationController {
     public String confirmDetails(@RequestBody UserDetails user){
         String ConfirmationMessage = String.format("Received details for %s with email %s", user.getName(), user.getEmail());
         return ConfirmationMessage;
+    }
+
+    //@PostMapping("/confirm")
+    public String notify(@RequestBody UserDetails user){
+
+        String notify = notificationServiceClient.SomeDetails(user);
+        return notify;
     }
 }
